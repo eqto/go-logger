e4bbad3aa4d1317f722b479a23bde13db93a990e
tuxer <e.yulian@gmail.com>
2020-09-09 10:52:49 +0700

    move level color to const

--- const.go
+++ const.go
@@ -19,3 +19,27 @@ const (
 	//LevelFatal ...
 	LevelFatal
 )
+
+const (
+	colorDebug = bgWhite + fgGreen
+	colorInfo  = bgWhite + fgBlue
+	colorWarn  = bgWhite + fgYellow
+	colorError = bgWhite + fgRed
+	colorFatal = bgRed + fgWhite
+)
+
+func levelColor(level int) string {
+	switch level {
+	case LevelDebug:
+		return colorDebug
+	case LevelInfo:
+		return colorInfo
+	case LevelWarn:
+		return colorWarn
+	case LevelError:
+		return colorError
+	case LevelFatal:
+		return colorFatal
+	}
+	return bgWhite + fgWhite
+}
